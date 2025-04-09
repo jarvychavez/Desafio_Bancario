f59fe71b5053fd55f7798d78fb39e1e1e341896c
jarvychavez <jav.chavez94@gmail.com>
2025-04-09 00:25:07 -0600

    fist commit

--- src/Desafio.java
+++ src/Desafio.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Desafio {
+public class Desafio {
+
+    public static void main(String[] args) {
+        String nombre = "Javier Chavez";
+        String tipoDeCuenta = "corriente";
+        double saldo = 2750;
+
+        System.out.println("************");
+        System.out.println("");
+    }
 }
