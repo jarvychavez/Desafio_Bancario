6ee882229e2249deb1ed5d2ae65c06e15bb195b6
jarvychavez <jav.chavez94@gmail.com>
2025-04-09 01:06:36 -0600

    menu con scanner y while

--- src/Desafio.java
+++ src/Desafio.java
@@ -1,14 +1,34 @@
+import java.util.Scanner;
+
 public class Desafio {
 
     public static void main(String[] args) {
         String nombre = "Javier Chavez";
-        String tipoDeCuenta = "corriente";
+        String tipoDeCuenta = "Ahorro";
         double saldo = 2750;
+        int opcion = 0;
 
-        System.out.println("************");
+        System.out.println("************************");
         System.out.println("\nNombre del cliente: " + nombre);
         System.out.println("El tipo de cuenta es: " + tipoDeCuenta);
         System.out.println("Su saldo es: " + "$"  + saldo);
+
+        System.out.println("\n************************");
+
+        String menu = """
+                *** Elija el número de la opción deseada ***
+                1- Consultar Saldo
+                2- Retirar
+                3- Depositar
+                7- Salir 
+                """;
+
+        Scanner teclado = new Scanner(System.in);
+        while (opcion != 7){
+            System.out.println(menu);
+            opcion = teclado.nextInt();
+
+        }
 
     }
 }
