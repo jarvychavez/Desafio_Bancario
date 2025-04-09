2e40bc6822c554daeea76482bf6cd9b864ddf76a
jarvychavez <jav.chavez94@gmail.com>
2025-04-09 00:38:57 -0600

    persona y descripcion

--- src/Desafio.java
+++ src/Desafio.java
@@ -6,6 +6,9 @@ public class Desafio {
         double saldo = 2750;
 
         System.out.println("************");
-        System.out.println("");
+        System.out.println("\nNombre del cliente: " + nombre);
+        System.out.println("El tipo de cuenta es: " + tipoDeCuenta);
+        System.out.println("Su saldo es: " + "$"  + saldo);
+
     }
 }
